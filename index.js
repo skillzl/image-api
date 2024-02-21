6ef9e16f6ac3763c879cec7249e52342ec321934
Catalin Ta <skillzlro@gmail.com>
2024-02-21 20:29:29 +0200

    📌 worked: secured api key & / endpoints

--- index.js
+++ index.js
@@ -27,8 +27,23 @@ app.use(limiter);
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.redirect('https://github.com/skillzl/image-api');
-})
+    const apps = [
+        {
+            name: "cat",
+            endpoint: "https://api.skillzl.dev/cat?key=YOUR_API_KEY"
+        },
+        {
+            name: "dog",
+            endpoint: "https://api.skillzl.dev/dog?key=YOUR_API_KEY"
+        },
+        {
+            name: "test",
+            endpoint: "https://api.eres.fun/test"
+        }
+    ];
+
+    res.json(apps);
+});
 
 app.get("/cat", (req, res) => {
     const key = req.query.key;
@@ -47,13 +62,14 @@ app.get("/cat", (req, res) => {
         res.header("Content-type", "application/json; charset=utf-8");
     }
     res.header("Content-type", "application/json; charset=utf-8");
-    if (api_key.includes(key)) {
+    if (key === api_key) {
         res.send(JSON.stringify(result, null, 2));
         console.log(result);
     } else {
-        const result = {}
-        result.code = 403;
-        result.message = "error: invalid api key, try again";
+        const result = {
+            code: 403,
+            message: 'error: invalid API key, try again',
+        };
         res.send(JSON.stringify(result, null, 2));
     }
 })
@@ -76,65 +92,16 @@ app.get("/dog", (req, res) => {
         res.header("Content-type", "application/json; charset=utf-8");
     }
     res.header("Content-type", "application/json; charset=utf-8");
-    if (api_key.includes(key)) {
+    if (key === api_key) {
         res.send(JSON.stringify(result, null, 2));
         console.log(result);
     } else {
-        const result = {}
-        result.code = 403;
-        result.message = "error: invalid api key, try again";
+        const result = {
+            code: 403,
+            message: 'error: invalid API key, try again',
+        };
         res.send(JSON.stringify(result, null, 2));
     }
-});
-
-app.get("/gifs", (req, res) => {
-    const key = req.query.key;
-    const result = {}
-
-    result.code = 200;
-
-    const randomNumber = Math.floor(Math.random() * 140) + 1;
-  
-    const url = `${process.env.GIFS_LINK + randomNumber}`;
-  
-    axios.get(url)
-    .then((response) => {
-      const $ = cheerio.load(response.data);
-      let images = [];
-      $('img').each((i, elem) => {
-        const src = $(elem).attr('src');
-        if (src !== "/settings/l8.gif") {
-          const gifSrc = src.replace(/\.(png|jpg)$/i, '.gif');
-          images.push(gifSrc);
-        }
-      });
-
-      images = images.filter(image => image !== "/settings/l8.gif");
-
-      const randomImage = images[Math.floor(Math.random() * images.length)];
-
-      if (!images.length) {
-        result.code = 404;
-        result.url = `error: no images available`;
-      } else {
-        result.url = `${randomImage}`;
-        result.nsfw = true;
-        result.key = key;
-        res.header("Content-type", "application/json; charset=utf-8");
-      }
-      res.header("Content-type", "application/json; charset=utf-8");
-      if (nfsw_key.includes(key)) {
-        res.send(JSON.stringify(result, null, 2));
-        console.log(result);
-      } else {
-        const result = {}
-        result.code = 403;
-        result.message = "error: invalid api key, try again";
-        res.send(JSON.stringify(result, null, 2));
-      }
-    }).catch((error) => {
-        console.error(error);
-    });
 });
 
 app.get("/test", (req, res) => {
